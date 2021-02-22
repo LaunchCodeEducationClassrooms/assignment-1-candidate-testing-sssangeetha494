35d8cbf40aa50106d1fb82dbd8d9e852c539bfc0
sssangeetha494 <sssangeetha494@gmail.com>
2021-02-22 01:50:05 +0000

    new commit 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question
+let question;
 question="Who was the first American woman in space? ";
-let correctAnswer
+let correctAnswer;
 correctAnswer="Sally Ride";
 let candidateAnswer="";
 let questions=["Who was the first American woman in space?",
